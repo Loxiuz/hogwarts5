41857cfce3ef5faf32b894f6ea114f4c4c16bc9a
Malte <91071750+Loxiuz@users.noreply.github.com>
2024-04-24 23:54:52 +0200

    student tests

--- src/main/java/dk/kea/dat3js/hogwarts5/students/Student.java
+++ src/main/java/dk/kea/dat3js/hogwarts5/students/Student.java
@@ -3,6 +3,7 @@ package dk.kea.dat3js.hogwarts5.students;
 import dk.kea.dat3js.hogwarts5.house.House;
 import jakarta.persistence.*;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 @Entity
@@ -45,7 +46,7 @@ public class Student {
   }
 
   public void setFirstName(String firstName) {
-    this.firstName = firstName;
+    this.firstName = capitalize(firstName);
   }
 
   public String getMiddleName() {
@@ -53,7 +54,14 @@ public class Student {
   }
 
   public void setMiddleName(String middleName) {
-    this.middleName = middleName;
+    if (middleName != null && middleName.contains(" ")) {
+      String[] names = middleName.split(" ");
+      String[] capitalizedNames = Arrays.stream(names).map(this::capitalize).toArray(String[]::new);
+      this.middleName = String.join(" ", capitalizedNames);
+    }
+    else {
+      this.middleName = capitalize(middleName);
+    }
   }
 
   public String getLastName() {
@@ -61,7 +69,7 @@ public class Student {
   }
 
   public void setLastName(String lastName) {
-    this.lastName = lastName;
+    this.lastName = capitalize(lastName);
   }
 
   public House getHouse() {
@@ -80,6 +88,29 @@ public class Student {
     this.schoolYear = schoolYear;
   }
 
+  public String getFullName(){
+    return firstName + " " + (middleName !=null ? middleName + " " : "") + lastName;
+  }
+
+  public void setFullName(String fullName){
+    if(fullName != null && !fullName.isEmpty()){
+      int firstSpace = fullName.indexOf(' ');
+      int lastSpace = fullName.lastIndexOf(' ');
+
+      setFirstName(fullName.substring(0, firstSpace));
+      if (firstSpace != lastSpace) {
+        setMiddleName(fullName.substring(firstSpace + 1, lastSpace));
+      } else {
+        setMiddleName(null);
+      }
+      setLastName(fullName.substring(lastSpace + 1));
+    } else {
+      setFirstName(null);
+      setMiddleName(null);
+      setLastName(null);
+    }
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
@@ -93,4 +124,17 @@ public class Student {
     return Objects.hash(getFirstName(), getMiddleName(), getLastName(), getHouse().getName());
   }
 
+  /************************
+  * Capitalization Feature
+  ************************/
+
+  public String capitalize(String name){
+    if(name != null && !name.isEmpty()){
+      String firstPart = name.substring(0, 1).toUpperCase();
+      String lastPart = name.substring(1).toLowerCase();
+      return firstPart + lastPart;
+    } else {
+      return null;
+    }
+  }
 }
---
+++ src/test/java/dk/kea/dat3js/hogwarts5/students/StudentTest.java
@@ -0,0 +1,131 @@
+package dk.kea.dat3js.hogwarts5.students;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @Test
+    void getFullNameWithMiddleName() {
+        Student s = new Student("Harry", "James", "Potter", null, 5);
+
+        String fullName = s.getFullName();
+        String expected = "Harry James Potter";
+
+        assertEquals(expected, fullName);
+    }
+
+    @Test
+    void getFullNameWithoutMiddleName() {
+        Student s = new Student("Cho", "Chang", null, 5);
+
+        String fullName = s.getFullName();
+        String expected = "Cho Chang";
+
+        assertEquals(expected, fullName);
+    }
+
+    @Test
+    void setFullNameWithMiddleName() {
+        Student s = new Student("first", "middle", "last", null, 5);
+
+        s.setFullName("Hermione Jean Granger");
+
+        String expectedFirst = "Hermione";
+        String expectedMiddle = "Jean";
+        String expectedLast = "Granger";
+
+        assertEquals(expectedFirst, s.getFirstName());
+        assertEquals(expectedMiddle, s.getMiddleName());
+        assertEquals(expectedLast, s.getLastName());
+    }
+
+    @Test
+    void setFullNameWithoutMiddleName() {
+        Student s = new Student("first", "middle", "last", null, 5);
+
+        s.setFullName("Cho Chang");
+
+        String expectedFirst = "Cho";
+        String expectedLast = "Chang";
+
+        assertEquals(expectedFirst, s.getFirstName());
+        assertEquals(expectedLast, s.getLastName());
+        assertNull(s.getMiddleName());
+    }
+
+    @Test
+    void setFullNameWithoutLastName() {}
+
+    @Test
+    void setFullNameWithMultipleMiddleNames() {
+        Student s = new Student("first", "middle", "last", null, 5);
+
+        s.setFullName("Albus Percival Wulfric Brian Dumbledore");
+
+        String expectedFirst = "Albus";
+        String expectedMiddle = "Percival Wulfric Brian";
+        String expectedLast = "Dumbledore";
+
+        assertEquals(expectedFirst, s.getFirstName());
+        assertEquals(expectedMiddle, s.getMiddleName());
+        assertEquals(expectedLast, s.getLastName());
+    }
+
+    @Test
+    void setFullNameWithEmptyString(){
+        Student s = new Student("first", "middle", "last", null, 5);
+
+        s.setFullName("");
+
+        assertNull(s.getFirstName());
+        assertNull(s.getMiddleName());
+        assertNull(s.getLastName());
+    }
+
+    @Test
+    void setFullNameWithNull(){
+        Student s = new Student("first", "middle", "last", null, 5);
+
+        s.setFullName(null);
+
+        assertNull(s.getFirstName());
+        assertNull(s.getMiddleName());
+        assertNull(s.getLastName());
+    }
+
+    @Test
+    void capitalizeIndividualNames(){
+        Student s = new Student("first", "middle", "last", null, 5);
+
+        s.setFirstName("harry");
+        s.setMiddleName("james");
+        s.setLastName("potter");
+
+        String expectedFirst = "Harry";
+        String expectedMiddle = "James";
+        String expectedLast = "Potter";
+
+        assertEquals(expectedFirst, s.getFirstName());
+        assertEquals(expectedMiddle, s.getMiddleName());
+        assertEquals(expectedLast, s.getLastName());
+    }
+
+    @Test
+    void capitalizeIndividualNamesWithCrazyCapitalization(){
+        Student s = new Student("first", "middle", "last", null, 5);
+
+        s.setFirstName("hARrY");
+        s.setMiddleName("jaMeS");
+        s.setLastName("poTtEr");
+
+        String expectedFirst = "Harry";
+        String expectedMiddle = "James";
+        String expectedLast = "Potter";
+
+        assertEquals(expectedFirst, s.getFirstName());
+        assertEquals(expectedMiddle, s.getMiddleName());
+        assertEquals(expectedLast, s.getLastName());
+    }
+}
